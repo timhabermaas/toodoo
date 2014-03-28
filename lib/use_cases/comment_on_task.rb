a31abebd30f595a1a5021eeecd2aa49068274e55
Tim Habermaas <tim.habermaas@gmail.com>
2014-03-29 00:50:56 +0100

    Extract behavior of mentioning users into method.

--- lib/use_cases/comment_on_task.rb
+++ lib/use_cases/comment_on_task.rb
@@ -7,22 +7,28 @@ class CommentOnTask < Struct.new(:database, :mailer, :current_user, :task_id, :c
 
     task = database.find Task, task_id
 
-    user_names = find_mentioned_users comment_form.content
-    mentions = user_names.map do |n|
+    usernames = find_mentioned_usernames comment_form.content
+    mentions = usernames.map do |n|
       database.query_user_by_name n
     end
 
-    comment = Comment.new(mentions: mentions, task: task, content: comment_form.content, author: current_user)
+    comment = Comment.new(task: task, content: comment_form.content, author: current_user)
 
-    mentions.each { |m| mailer.send_mention_in_comment_mail(m, comment) }
-    mentions.each { |m| task.add_follower m }
+    mentions.each do |m|
+      mention_user_in_comment m, comment
+    end
 
     database.create comment
     comment
   end
 
   private
-    def find_mentioned_users text
+    def find_mentioned_usernames text
       text.scan(/@[a-zA-Z]+/).map { |n| n[1..-1] }
+    end
+
+    def mention_user_in_comment user, comment
+      mailer.send_mention_in_comment_mail user, comment
+      comment.task.add_follower user
     end
 end
